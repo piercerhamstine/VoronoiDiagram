294b881a55ff9b1559ea1629da2fde83736bd91d
pierce <piercerhamstine@gmail.com>
2022-12-09 00:03:07 -0600

    Write file to disk

--- main.c
+++ main.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define _height 400
 #define _width 400
 
 static unsigned int pixels[_height][_width] = {0};
 
+void fillImage();
+void saveImageToDisk();
+
 int main(void)
 {
+    saveImageToDisk();
+
     return 0;
+};
+
+void fillImage()
+{
+    for(size_t i = 0; i < _height; ++i)
+    {
+        for(size_t j = 0; j < _width; ++j)
+        {
+        };
+    };
+};
+
+void saveImageToDisk()
+{
+    FILE *file;
+
+    file = fopen("image.ppm", "w");
+
+    if(file == NULL)
+    {
+        printf("Failed to open file.");
+        exit(1);  
+    };
+
+    fclose(file);
 };
